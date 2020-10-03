3473921468d5c5212277ee11cc493b7630d86e45
Diana Dashkovskaya <dianadashkovskaya@MacBook-Pro-Diana.local>
2020-10-03 12:51:19 +0200

    final commit

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,28 @@
 // Write code here to communicate with Github
+window.onload = getRepos;
+
+function getListofRepos(user = "hheiress") {
+  return fetch("https://api.github.com/users/" + user + "/repos")
+    .then(function(responce) {
+      //can replace it into ES6: responce=>{return responce.json()}
+      return responce.json();
+    })
+    .then(data => {
+      return data.map(item => {
+        return item.name;
+      });
+    });
+}
+
+function getRepos() {
+  let getList = document.querySelector("#repos-list");
+  getList.innerHTML = "";
+  let listOfRepo = getListofRepos("hheiress");
+  listOfRepo.then(resp => {
+    resp.forEach(item => {
+      let li = document.createElement("li");
+      li.innerText = item;
+      getList.appendChild(li);
+    });
+  });
+}
